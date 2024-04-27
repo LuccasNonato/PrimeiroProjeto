004b2d48e3a89930e392827184fbaf93e30afd18
Luccas <luccasjunqueira@hotmail.com>
2024-04-27 14:21:38 -0300

    first studies

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,7 +1,30 @@
-﻿internal class Program
+﻿using System.Globalization;
+using System.Net.Security;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        int anos = 0;
+        string nome;
+        Double valor;
+
+        bool Verdadeiro = true;
+
+        if (Verdadeiro == true)
+        {
+            nome = "Luccas";
+            valor = 10.2354;
+            anos = 27;
+
+            Console.WriteLine(nome + " tem " + anos + " anos e apenas " + valor.ToString("F2",CultureInfo.InvariantCulture) + " reais");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+
+
         Console.WriteLine("Hello, World!");
     }
 }
