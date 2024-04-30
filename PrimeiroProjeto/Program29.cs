d523d04775e3fe41a9701109376b4ffc5b1b7e6c
Luccas <luccasjunqueira@hotmail.com>
2024-04-29 21:32:58 -0300

    29-04

--- PrimeiroProjeto/Program29.cs
+++ PrimeiroProjeto/Program29.cs
@@ -2,27 +2,6 @@
 using System.Net.Security;
 using System.Numerics;
 
-internal class Program
+internal class Program : ProgramBase
 {
-    private static void Main(string[] args)
-    {
-        int a = 50;
-       
-        
-        bool c1 = a < 10;
-        bool c2 = a < 20;
-        bool c3 = a < 5;
-        bool c4 = a > 5;
-        bool c5 = a == 10;
-
-        Console.WriteLine(c1);
-        Console.WriteLine(c2);
-        Console.WriteLine(c3);
-        Console.WriteLine(c4);
-        Console.WriteLine(c5);
-
-
-
-
-    }
 }
