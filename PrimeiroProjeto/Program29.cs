b8f73b456b1f1d518cfee2623b66dd977a7ea3ec
Luccas <luccasjunqueira@hotmail.com>
2024-04-29 22:07:00 -0300

    29-04 MOD

--- PrimeiroProjeto/Program29.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Globalization;
-using System.Net.Security;
-using System.Numerics;
-
-internal class Program : ProgramBase
-{
-}
---
+++ dia29-04/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool b4 = 4 != 5;
+
+bool b5 = 4 > 3 && 5 < 4; // && || == !=
+Console.WriteLine(b4);
+Console.WriteLine(b5);
+
+Console.WriteLine("Digita um número");
+
+int x = int.Parse(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("par!");
+
+}
+else
+{
+    Console.WriteLine("impar!");
+}
+
+
